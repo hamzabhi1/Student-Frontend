6d20985761214f953ed7b3e5eda0f9192e5ca0e2
Hamza <hamzabhilegend002@gmail.com>
2026-05-05 12:00:04 -0700

    fixed vite config and deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import store from "./store/store.js";
 import { Provider } from "react-redux";
+import { HashRouter } from "react-router-dom";
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={store}>
         <App />
         <ToastContainer />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
