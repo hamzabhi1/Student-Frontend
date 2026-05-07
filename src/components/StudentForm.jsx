5f4958cf3d9ce2acebd5c9f0fc2668abea074991
Hamza <hamzabhilegend002@gmail.com>
2026-05-07 08:37:36 -0700

    dfd

--- src/components/StudentForm.jsx
+++ src/components/StudentForm.jsx
@@ -14,7 +14,7 @@ const StudentForm = ({ onSubmit, loading, initialData, onCancel }) => {
     );
     const [profilePicture, setProfilePicture] = useState(null);
     const [previewUrl, setPreviewUrl] = useState(
-        initialData?.profilePicture ? `https://student-backend-fxbfeqy4e-hamzabhi1s-projects.vercel.app/uploads/${initialData.profilePicture}` : null
+        initialData?.profilePicture ? `https://student-backend-ll1dei3pf-hamzabhi1s-projects.vercel.app/uploads/${initialData.profilePicture}` : null
     );
 
     const handleChange = (e) => {
